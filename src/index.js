b6bcf4e05bcdc2bd390054e37032c09767eaad0f
maoforero <maforeror@gmail.com>
2022-01-26 23:21:55 -0500

    creando router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
 
+//router
+const router = require('./routes');
 
 app.use('/', (req, res) => {
-  res.send('Server')
+  res.send('Server');
 })
+
+app.use('/', router)
 
 app.listen(PORT, (req, res) => {
   console.log(`It's Alive 🤖 http://localhost:${PORT}`)
