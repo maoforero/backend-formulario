c45c7760f2e329bab5cce034996bb2ccda9d5848
maoforero <maforeror@gmail.com>
2022-01-29 01:12:06 -0500

    creating db connection

---
+++ src/dbconnection/connection.js
@@ -0,0 +1,8 @@
+const mongooser = require('mongoose');
+
+const password = mao2656;
+const username = mao;
+
+const uri =  `mongodb+srv://<${username}>:<${password}>@dbch.yynea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+module.exports = () =>   mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
