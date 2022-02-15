ef54112b4b746b4093995ef1fb8e12990c134e66
maoforero <maforeror@gmail.com>
2022-02-15 01:58:05 -0500

    global dontenv

---
+++ src/controllers/info.controlller.js
@@ -0,0 +1,7 @@
+const infoController = {};
+
+infoController.getInfo = (req, res) => {
+  res.send('getting');
+}
+
+module.exports = infoController;
--- src/dbconnection/connection.js
+++ src/dbconnection/connection.js
@@ -6,13 +6,3 @@ const username = "pepe";
 const uri = `mongodb+srv://<${username}>:<${password}>@dbch.yynea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 module.exports = () =>   mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-
-
-// const { MongoClient } = require('mongodb');
-// const uri = "mongodb+srv://<username>:<password>@dbch.yynea.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
